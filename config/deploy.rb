ba8bad1cc92fa2b97231d47471b95f0d0ee4e656
nishi0401 <s1y0u1u7taiko@gmail.com>
2024-06-11 15:58:59 +0900

    自動デプロイの設定

---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+lock '3.18.1'
+set :application, 'japan_complete'
+set :repo_url,  'git@github.com:nishi0401/japan_complete.git'
+set :branch, 'main'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rbenv_type, :user
+set :rbenv_ruby, '3.2.0'
+set :ssh_options, auth_methods: ['publickey'],
+                          keys: ['~/.ssh/my-key-pair.pem'] 
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
