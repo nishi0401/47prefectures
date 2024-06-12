16d92f9c74be79b61fdd08e87dbceda1bc785288
nishi0401 <s1y0u1u7taiko@gmail.com>
2024-06-12 17:30:13 +0900

    ログアウト時のエラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
     resources :destinations, only: [:index]
   end
   root to: "records#index"
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
