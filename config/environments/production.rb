3623ff08b3fb285078690c4ad7617610c222b2e8
nishi0401 <s1y0u1u7taiko@gmail.com>
2024-06-13 14:59:11 +0900

    パスワード再設定ページの編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: "localhost"}
+  config.action_mailer.default_url_options = { host: "52.194.67.144"}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => 'ai2647995@gmail.com',
+    :password => 'mpxy lkbr jjpd qpne',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
 end
