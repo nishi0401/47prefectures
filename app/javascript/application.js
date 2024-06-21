42dadf750079cead43479b301dc378d951ab978d
nishi0401 <s1y0u1u7taiko@gmail.com>
2024-06-21 17:36:46 +0900

    エラーの解消

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"
