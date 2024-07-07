41290e3db74ffe659c5c43f44093d7c0077ce75b
nishi0401 <s1y0u1u7taiko@gmail.com>
2024-07-07 17:25:40 +0900

    Userモデルの単体テストコードの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,50 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'ユーザー新規登録' do
     context '新規登録できるとき' do
       it 'すべての項目の条件を満たしていれば登録できる'do
-        user = User.new(nickname: 'taro', email: 'test@example', password: '000000', password_confirmation: '000000')
-        expect(user).to be_valid
+        expect(@user).to be_valid
       end
     end
     context '新規登録できないとき' do
       it 'nicknameが空では登録できない' do
-        user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
-        user.valid?
-        expect(user.errors.full_messages).to include("Nicknameを入力してください")
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nicknameを入力してください")
       end
       it 'emailが空では登録できない' do
-        user = User.new(nickname: 'taro', email: '', password: '000000', password_confirmation: '000000')
-        user.valid?
-        expect(user.errors.full_messages).to include("Eメールを入力してください")
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
       end  
       it '同じemailは登録できない' do
-        user = User.new(nickname: 'taro', email: 'test@example', password: '000000', password_confirmation: '000000')
-        user.save
-        another_user = User.new(nickname: 'jiro', email: 'test@example', password: '000000', password_confirmation: '000000')
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
         another_user.valid?
         expect(another_user.errors.full_messages).to include('Eメールはすでに存在します')
       end
       it 'emailは@を含まないと登録できない' do
-        user = User.new(nickname: 'taro', email: 'aaaaaaaa', password: '000000', password_confirmation: '000000')
-        user.valid?
-        expect(user.errors.full_messages).to include("Eメールは不正な値です")
+        @user.email = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Eメールは不正な値です")
       end
       it 'パスワードが空では登録できない' do
-        user = User.new(nickname: 'taro', email: 'test@example', password: '', password_confirmation: '000000')
-        user.valid?
-        expect(user.errors.full_messages).to include("パスワードを入力してください")
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードを入力してください")
       end
       it 'パスワードが6文字未満では登録できない' do
-        user = User.new(nickname: 'taro', email: 'test@example', password: '00000', password_confirmation: '00000')
-        user.valid?
-        expect(user.errors.full_messages).to include("パスワードは6文字以上で入力してください")
+        @user.password = 'aaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードは6文字以上で入力してください")
       end
-      it '確認用パスワードが空では登録できない' do
-        user = User.new(nickname: 'taro', email: 'test@example', password: '000000', password_confirmation: '')
-        user.valid?
-        expect(user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+      it 'パスワードと確認用パスワードが一致しないと登録できない' do
+        @user.password_confirmation = '00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
       end
     end
   end
